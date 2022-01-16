ef9e32427c84ae5b9140344567d144c6ef468be6
TheKetrab <ketrab508@gmail.com>
2022-01-16 11:29:58 +0100

    asd

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ const server_port = 8080;
 app.get("/:a/:b", (req, res) => {
     const a = parseInt(req.params["a"],10);
     const b = parseInt(req.params["b"],10);
-    res.send(`${a} + ${b} = ${sum(a,b)} (version 3)`)
+    res.send(`${a} + ${b} = ${sum(a,b)} (version 4)`)
 });
 
 app.listen(server_port, () => console.log(`app listening on port ${server_port}`));
